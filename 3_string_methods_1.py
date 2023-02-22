633f2454df73257c0b9b11bf823073c615177908
sudeshna2209 <125039157+sudeshna2209@users.noreply.github.com>
2023-02-22 20:37:02 +0530

    Add files via upload

---
+++ 3_string_methods_1.py
@@ -0,0 +1,156 @@
+# print(dir(str))
+
+# x="hELLo WorLd"
+# print(x.capitalize())
+
+# a="sAI SudeSHNa"
+# print(a.capitalize())
+# print(type(a))
+# print(len(a))
+
+# n="HEllo PyThOn"
+# print(n.casefold())
+
+# x="PyThOn PrOgrAmmInG"
+# print(x.lower())
+
+# x="Hello python"
+# print(len(x))
+
+# a="hello world"
+# print(a.center(20))
+# print(a.center(20,'&'))
+
+# a="hEllo PyThON"
+# print(type(a))
+# n=a.encode()
+# print(n,type(n))
+# print(n.decode())
+
+# name="HeLlo PyThON"
+# print(name.upper())
+# print(name.isupper())
+# print(name.lower())
+# print(name.islower())
+
+# n="FOOTBALL"
+# print(n.upper())
+# print(n.isupper())
+# print(n.lower())
+# print(n.islower())
+
+# n="basketball"
+# print(n.upper())
+# print(n.isupper())
+# print(n.lower())
+# print(n.islower())
+
+# x="hELLo pYtHON"
+# print(x.title())
+# print(x.capitalize())
+# print(x.istitle())
+# x="Hello World"
+# print(x.istitle())
+
+# a="hello python"
+# print(a.endswith(''))
+# print(a.endswith('  '))
+# print(a.endswith('n'))
+# print(a.endswith('o'))
+
+# x="PyThOn PrOgrAmminG"
+# print(x.startswith(''))
+# print(x.startswith('P'))
+# print(x.startswith('  '))
+# print(x.startswith('p'))
+
+# a='    '
+# b=' hello python   '
+# print(a.isspace())
+# print(b.isspace())
+
+# q="hEllo pyo1Tholno"
+# print(q.index('l'))
+# print(q.index('l',4))
+# print(q.index('o'))
+# print(q.index('o',5))
+# print(q.rindex('h'))
+# print(q.index('1'))
+# print(q.index('b'))
+
+# r="hEllo pyThon programming"
+# # print(r.index('u')) 
+# print(r.rindex('n'))
+# print(len(r))
+# print(r.rindex('g'))
+
+# a="sudeshna123"
+# print(a.isidentifier())
+# b="1demo"
+# print(b.isidentifier())
+
+# x="python programming"
+# print(x.find('t'))
+# print(x.find('s'))
+
+# a="Hello python"
+# print(a.count('p')) 
+# print(a.count('m')) 
+# print(a.count('l')) 
+# print(a.count('e')) 
+
+# a="python programming"
+# a[0]='P'
+# print(a.replace('programming','course'))
+
+# x="domain python programming domain"
+# print(x.replace('domain',2))
+# print(x.replace('domain',str(1)))
+# print(x.replace('domain','2'))
+# print(x.replace('o','s',1))
+# print(x.replace('o','s',2))
+# print(x.replace('o','s',4))
+# print(x.replace('o','s',6))
+# print(x.replace('o','s',(2)))
+
+# x="        Hello Python    .programming       "
+# print(x.strip())
+
+# print(x.lstrip())
+# print(x.rstrip())
+
+# a="hello python"
+# print(a.isalnum())
+
+# b="hello python123"
+# print(b.isalnum())
+
+# c="python1234"
+# print(c.isalnum())
+
+# a="H"
+# print(a.isalpha())
+
+# a="suddu123"
+# print(a.isalnum())
+
+# a="PYthOn pROgRaMMing"
+# print(a.swapcase())
+
+# a="hello"
+# print(a.zfill(7))
+
+# a="Hello welcome to python programming"
+# print(a.split())
+
+# b="welcome*to*c"
+# print(b.split("*"))
+
+# v="hello\nwelcome\nto\nthe\nprogramming\nworld"
+# print(v.splitlines())
+
+# a='hello','python','programming'
+# n='python'
+# print('-'.join(a))
+# print('*'.join(n))
+# print('*'.join('Hello'))
